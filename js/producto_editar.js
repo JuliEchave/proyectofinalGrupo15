41bf0c5a015914db2a05f29daa4d3f1a2a0caaff
Franco Tortolini <132412488+fjtortolini@users.noreply.github.com>
2023-07-03 11:26:28 -0300

    Update producto_editar.js

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -1,63 +1,64 @@
-console.log(location.search)     // lee los argumentos pasados a este formulario
-var id=location.search.substr(4)
-console.log(id)
-const { createApp } = Vue
-  createApp({
-    data() {
-      return {
-        id:0,
-        modelo:"",
-        talle:"",
-        stock:0,
-        precio:0,
-        imagen:"",
-        url:'http://127.0.0.1:5000/remera'+id,
-       }  
+console.log(location.search); // lee los argumentos pasados a este formulario
+var id = location.search.substr(4);
+console.log(id);
+
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      id: 0,
+      modelo: "",
+      talle: "",
+      stock: 0,
+      precio: 0,
+      imagen: "",
+      url: 'http://127.0.0.1:5000/remera/' + id,
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.id = data.id;
+          this.modelo = data.modelo;
+          this.talle = data.talle;
+          this.stock = data.stock;
+          this.precio = data.precio;
+          this.imagen = data.imagen;
+        })
+        .catch(err => {
+          console.error(err);
+          this.error = true;
+        });
     },
-    methods: {
-        fetchData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    this.id=data.id
-                    this.modelo= data.modelo;
-                    this.talle= data.talle,
-                    this.stock=data.stock,
-                    this.precio=data.precio,
-                    this.imagen=data.imagen                    
-                })
-                .catch(err => {
-                    console.error(err);
-                    this.error=true              
-                })
-        },
-        modificar() {
-            let remera = {
-                modelo:this.modelo,
-                talle:this.talle,
-                stock: this.stock,
-                precio: this.precio,
-                imagen:this.imagen
-            }
-            var options = {
-                body: JSON.stringify(remera),
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow'
-            }
-            fetch(this.url, options)
-                .then(function () {
-                    alert("Registro modificado")
-                    window.location.href = "./productos.html";             
-                })
-                .catch(err => {
-                    console.error(err);
-                    alert("Error al Modificar")
-                })      
-        }
+    modificar() {
+      let remera = {
+        modelo: this.modelo,
+        talle: this.talle,
+        stock: this.stock,
+        precio: this.precio,
+        imagen: this.imagen,
+      };
+      var options = {
+        body: JSON.stringify(remera),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow',
+      };
+      fetch(this.url, options)
+        .then(() => {
+          alert("Registro modificado");
+          window.location.href = "./productos.html";
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Error al Modificar");
+        });
     },
-    created() {
-        this.fetchData(this.url)
-    },
-  }).mount('#app')
+  },
+  created() {
+    this.fetchData(this.url);
+  },
+}).mount('#app');
